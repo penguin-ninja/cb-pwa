125507fe9e18c1b9711bd265bd737a5488978137
Riley Criddle <rileycriddle220@gmail.com>
2018-08-22 01:22:20 +0800

    feat(devices): device enumerative values and config apis

--- src/app/stores/devices-store.js
+++ src/app/stores/devices-store.js
@@ -1,4 +1,5 @@
-import { observable, action } from 'mobx';
+import { observable, action, computed, extendObservable } from 'mobx';
+import BATCH_TYPE_ENUMS from 'app/constants/BatchTypeEnums';
 
 class DevicesStore {
   @observable
@@ -8,23 +9,104 @@ class DevicesStore {
 
   constructor(rootStore) {
     this.rootStore = rootStore;
+    this.loadEnums();
+    this.loadDevices();
   }
 
   get api() {
     return this.rootStore.authStore;
   }
 
+  get plantId() {
+    return this.rootStore.authStore.plantId;
+  }
+
+  @computed
+  get devicesByType() {
+    return this.deviceTypes.map(type =>
+      this.devices.filter(d => d.deviceTypeName === type)
+    );
+  }
+
+  // devices CRUD
   @action
-  loadDeviceTypes() {
+  loadDevices() {
     return this.api
-      .makeAuthorizedRequest('/api/batch/BatchType/deviceTypeName')
+      .makeAuthorizedRequest(`/api/batch/DeviceManager/plant/${this.plantId}`)
       .then(resp => {
-        this.deviceTypes = resp;
+        this.devices = resp;
       });
   }
 
   @action
-  loadDevicesConfigurations() {}
+  createDevice(deviceType, device) {
+    return this.api
+      .makeAuthorizedRequest(
+        `/api/batch/DeviceManager/plant/${this.plantId}/${deviceType}`,
+        {
+          [deviceType]: device
+        },
+        'POST'
+      )
+      .then(() => {
+        this.devices[deviceType] = this.devices[deviceType] || [];
+        this.devices[deviceType].push(device);
+      });
+  }
+
+  @action
+  updateDevice(deviceId, deviceType, device) {
+    // NOTE check if this API requires all properteis
+    return this.api
+      .makeAuthorizedRequest(
+        `/api/batch/DeviceManager/${deviceType}/${deviceId}`,
+        {
+          [deviceType]: device
+        },
+        'PUT'
+      )
+      .then(() => {
+        const index = this.devices[deviceType].findIndex(
+          d => d.id === deviceId
+        );
+
+        if (index > -1) {
+          extendObservable(this.devices[deviceType][index], device);
+        }
+      });
+  }
+
+  @action
+  deleteDevice(deviceId, deviceType) {
+    return this.api
+      .makeAuthorizedRequest(
+        `/api/batch/DeviceManager/${deviceId}`,
+        null,
+        'DELETE'
+      )
+      .then(() => {
+        this.devices[deviceType] = this.devices[deviceType].filter(
+          d => d.id !== deviceId
+        );
+      });
+  }
+
+  // Device Enums
+  @action
+  loadEnumConfiguration(key) {
+    return this.api
+      .makeAuthorizedRequest(`/api/batch/BatchType/${key}`)
+      .then(resp => {
+        this[key] = resp;
+      });
+  }
+
+  @action
+  loadEnums() {
+    return Promise.all(
+      BATCH_TYPE_ENUMS.map(type => this.loadEnumConfiguration(type))
+    );
+  }
 }
 
 export default DevicesStore;
