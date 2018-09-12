cef908e2b56465493cb43482c241401155e4c0ae
Riley Criddle <rileycriddle220@gmail.com>
2018-09-12 19:11:35 +0800

    update(gui): use react-rnd on hoc

--- src/app/hocs/deviceDOMHoc.js
+++ src/app/hocs/deviceDOMHoc.js
@@ -1,30 +1,46 @@
 import React from 'react';
+import { Rnd } from 'react-rnd';
 import './deviceDOMHoc.css';
 
 export default function deviceDOMHoc(WrappedComponent) {
   return class extends React.Component {
-    renderEdit() {
-      const { x, y, width, height } = this.props;
-      const style = {
-        x: x + width + 20,
-        y: y + height - 10
-      };
+    onDragStop = (e, d) => {
+      this.props.onUpdate({
+        x: d.x,
+        y: d.y
+      });
+    };
 
-      return <i className="fa fa-pencil device-dom__edit" style={style} />;
+    onResize = (e, direction, ref, delta, position) => {
+      this.props.onUpdate({
+        length: ref.style.width.replace('px', '') * 1,
+        size: ref.style.height.replace('px', '') * 1,
+        ...position
+      });
+    };
+
+    renderEdit() {
+      const { onEdit } = this.props;
+
+      return <i className="fa fa-pencil device-dom__edit" onClick={onEdit} />;
     }
 
     render() {
-      const { width, height } = this.props;
-      const style = {
-        width,
-        height
-      };
+      const { width, height, x, y } = this.props;
+      const position = { x, y };
+      const size = { width, height };
 
       return (
-        <div className="device-dom__wrapper" style={style}>
+        <Rnd
+          className="device-dom__wrapper"
+          position={position}
+          size={size}
+          onDragStop={this.onDragStop}
+          onResize={this.onResize}
+        >
           <WrappedComponent {...this.props} />
           {this.renderEdit()}
-        </div>
+        </Rnd>
       );
     }
   };
