a938e81bd3977e032bebfd4c568b09278c0175ff
Riley Criddle <rileycriddle220@gmail.com>
2018-09-19 07:19:56 +0800

    feat(device): provide resizing and dragging as prop to device

--- src/app/hocs/deviceDOMHoc.js
+++ src/app/hocs/deviceDOMHoc.js
@@ -4,19 +4,44 @@ import './deviceDOMHoc.css';
 
 export default function deviceDOMHoc(WrappedComponent) {
   return class extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        dragging: false,
+        resizing: false
+      };
+    }
+
+    onDrag = () => {
+      this.setState({ dragging: true });
+    };
+
     onDragStop = (e, d) => {
-      this.props.onUpdate({
-        x: d.x,
-        y: d.y
-      });
+      setTimeout(() => {
+        this.setState({ dragging: false });
+      }, 100);
+
+      if (d.x !== this.props.x || d.y !== this.props.y) {
+        this.props.onUpdate({
+          x: d.x,
+          y: d.y
+        });
+      }
     };
 
     onResize = (e, direction, ref, delta, position) => {
+      this.setState({ resizing: true });
       this.props.onUpdate({
         length: ref.style.width.replace('px', '') * 1,
         size: ref.style.height.replace('px', '') * 1,
         ...position
       });
+    };
+
+    onResizeStop = () => {
+      setTimeout(() => {
+        this.setState({ resizing: false });
+      }, 100);
     };
 
     renderDesc() {
@@ -53,6 +78,7 @@ export default function deviceDOMHoc(WrappedComponent) {
 
     render() {
       const { width, height, x, y } = this.props;
+      const { dragging, resizing } = this.state;
       const position = { x, y };
       const size = { width, height };
 
@@ -64,10 +90,16 @@ export default function deviceDOMHoc(WrappedComponent) {
           minWidth={100}
           minHeight={30}
           onDragStop={this.onDragStop}
+          onDrag={this.onDrag}
           onResize={this.onResize}
+          onResizeStop={this.onResizeStop}
         >
           {this.renderDesc()}
-          <WrappedComponent {...this.props} />
+          <WrappedComponent
+            {...this.props}
+            dragging={dragging}
+            resizing={resizing}
+          />
           {this.renderEdit()}
           {this.renderDelete()}
         </Rnd>
